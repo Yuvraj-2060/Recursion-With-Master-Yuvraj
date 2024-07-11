d031401de6e204b0c68be2252516c62d0c222c4e
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-07-11 11:11:44 +0530

    Added another implementation. This time withoit passsing ArrayList in the method call.

--- src/Linear_Search_GetTargetIndex.java
+++ src/Linear_Search_GetTargetIndex.java
@@ -5,6 +5,7 @@ public class Linear_Search_GetTargetIndex {
         int arr[] = {5,6,1,2,4,4,7};
 
         System.out.println(getTargetIndex(arr, 0, 4));
+        System.out.println(findAllIndex(arr, 0, 4));
     }
 
     private static ArrayList<Integer>  getTargetIndex(int[] arr, int i, int target) {
@@ -14,12 +15,27 @@ public class Linear_Search_GetTargetIndex {
         return ls;
 
     }
-
+    // 1). By Passing ArrayList
     private static ArrayList<Integer> getTargetList(int[] arr, int index, int target, ArrayList<Integer> ls) {
         if(index==arr.length)
             return ls;
         if(arr[index]==target)
             ls.add(index);
         return getTargetList(arr, index+1, target, ls);
+    }
+
+
+    // 2). Without Passing The ArrayList
+    private static ArrayList<Integer> findAllIndex(int[] arr, int index, int target) {
+        ArrayList<Integer> ls = new ArrayList<>();
+        if(index == arr.length)
+            return ls;
+        if(arr[index] == target)
+            ls.add(index);
+
+        ArrayList<Integer> ansFromBelowCalls = findAllIndex(arr, index+1,target);
+        ls.addAll(ansFromBelowCalls);
+
+        return ls;
     }
 }
